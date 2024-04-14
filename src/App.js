7aa156bffc753a94cc5c0d609ec7b2ba3f693957
otienorichard <otienorichard505@gmail.com>
2024-04-14 12:55:07 +0300

    Adding the links

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
         <div>
           <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>}>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='*' element={<div>404 Not Found</div>}></Route>
 
-        </Route>
       </Routes>
         </div>
       </Router>
--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -35,8 +35,75 @@ function Navbar() {
     return (
         <nav className={`navbar ${navActive ? "active" : ""}`}>
             <div>
-                <img src="./img/logo.png" alt="logo image" />
+                <img className="navbar--logo" src="./img/logo.png" alt="logo image" />
             </div>
+            <a className={`nav__hamburger  ${navActive? "active" : ""}`} onClick={toggleNav}>
+                <span className="nav__hamburger__line"></span>
+                <span className="nav__hamburger__line"></span>
+                <span className="nav__hamburger__line"></span>
+            </a>
+
+            <div className={`navbar--items ${navActive ? "acrive" : ""}`}>
+                <ul>
+                    <li>
+                        <Link onClick={closeMenu}
+                        activeClass="navbar--active-content"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                        to="heroSection"
+                        className="navbar--content"
+                        >Home</Link>
+                    </li>
+
+                    <li>
+                        <Link onClick={closeMenu}
+                        activeClass="navbar--active-content"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                        to="MyPortfolio"
+                        className="navbar--content"
+                        >Portfolio</Link>
+                    </li>
+
+                    <li>
+                        <Link onClick={closeMenu}
+                        activeClass="navbar--active-content"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                        to="AboutMe"
+                        className="navbar--content"
+                        >About Me</Link>
+                    </li>
+
+                    <li>
+                        <Link onClick={closeMenu}
+                        activeClass="navbar--active-content"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                        to="Testimonials"
+                        className="navbar--content"
+                        >Testimonials</Link>
+                    </li>
+                </ul>
+            </div>
+            <Link
+            onClick={closeMenu}
+            activeClass="navbar--active-content"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            to="Contact"
+            className="btn btn-outline-primary"
+            >Contact Me</Link>
         </nav>
     );
 }
