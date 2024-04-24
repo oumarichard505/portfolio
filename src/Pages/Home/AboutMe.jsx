ac3033cdad12c7b6b0becc6d507874405d088025
otienorichard <otienorichard505@gmail.com>
2024-04-24 19:14:31 +0300

    Adding data

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -2,24 +2,16 @@
     return (
       <section id="AboutMe" className="about--section">
         <div className="about--section--img">
-          <img src="./img/about-me.png" alt="About Me" />
+          <img src="./img/hero_img.jpg" alt="About Me" />
         </div>
         <div className="hero--section--content--box about--section--box">
           <div className="hero--section--content">
-            <p className="section--title">About</p>
+ 
             <h1 className="skills-section--heading">About Me</h1>
             <p className="hero--section-description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-              officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-              recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
-              tempora numquam sint odit optio.
+"I am a Software Engineer with a background in both traditional education from Moringa School and alternative education from ALX/Holberton School. My experience includes working on various projects, both independently and collaboratively, honing my skills through internships and roles at companies like Italanta, Digital Divide Data, Limax, and Impact Outsourcing AI. While proficient in software engineering, I am also eager to explore the intersection of machine learning with my current expertise. Known for my adaptability, diligence, and problem-solving abilities, I am committed to delivering innovative solutions in the tech industry."
             </p>
-            <p className="hero--section-description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-              officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-              recusandae?
-            </p>
-          </div>
+             </div>
         </div>
       </section>
     );
